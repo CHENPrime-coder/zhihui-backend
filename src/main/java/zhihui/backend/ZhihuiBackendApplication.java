390851da10043c20bd5cea8420ad78890aee4237
CHEN Prime <chenbprime@outlook.com>
2023-02-19 18:48:00 +0800

    init project

---
+++ src/main/java/zhihui/backend/ZhihuiBackendApplication.java
@@ -0,0 +1,16 @@
+package zhihui.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author CHENPrime-coder
+ */
+@SpringBootApplication
+public class ZhihuiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhihuiBackendApplication.class, args);
+    }
+
+}
